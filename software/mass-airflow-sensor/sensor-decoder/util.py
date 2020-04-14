47513d209ed4c4feb032fb41bb2be9d76bfb621c
maehw <weber-m@posteo.de>
2020-04-14 21:15:00 +0200

    Mass airflow sensor decoder/analyzer: improved Python decoder/analyzer

--- software/mass-airflow-sensor/sensor-decoder/util.py
+++ software/mass-airflow-sensor/sensor-decoder/util.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 # Settings constants.
 DEVICE_TYPE_SETTING = 'Device type'
 I2C_BUS_ADDRESS_SETTING = 'I2C bus address'
@@ -5,6 +7,10 @@ I2C_BUS_ADDRESS_SETTING = 'I2C bus address'
 DEVICE_TYPE_CHOICES = {
   'Sensirion SFM3000': 0
 }
+
+class SensorMode(Enum):
+    GET_SERIAL = 1
+    GET_MEASUREMENT = 2
 
 # This HLA only uses I2C analyzer frames as input and will produce frames for every valid transaction (from I2C start condition to stop condition).
 class FlowMeterHla():
@@ -63,97 +69,107 @@ class FlowMeterHla():
         if data["type"] == "start":
             # ignore if already started -> always re-start new packet
 
-            # set start time
-            self.temp_frame["start_time"] = data["start_time"]
-            
             # mark frame as started
             self.frame_started = True
             # reset frame data
-            self.temp_frame["data"]["address"] = None
+            self.temp_frame = {
+                'type' : 'invalid',
+                'start_time' : data['start_time'],
+                'end_time' : data['end_time'],
+                'data' : {
+                    'address' : None,
+                    'command' : None,
+                    'raw' : None,
+                    'flow' : None
+                }
+            }
             self.frame_data = bytearray()
 
         if data["type"] == "address":
-            # TODO: sanity-check if frame has started
-            # TODO: sanity-check if address has already been set for the frame;
-            #       if so, set packet to error
-            
-            # set address
-            address_byte = data["data"]["address"][0]
-            self.temp_frame["data"]["address"] = hex(address_byte)
+            # sanity-check if frame has started
+            if self.frame_started:
+                # TODO: sanity-check if address has already been set for the frame;
+                #       if so, set packet to error
+                
+                # set address
+                address_byte = data['data']['address'][0]
+                self.temp_frame['data']['address'] = hex(address_byte)
 
         if data["type"] == "data":
-            # TODO: sanity-check if frame has started
-            # TODO: sanity-check if frame's address has been set
+            # sanity-check if frame has started
+            if self.frame_started:
+                # TODO: sanity-check if frame's address has been set
 
-            # append data
-            data_byte = data["data"]["data"][0]
-            self.frame_data.append( data_byte )
+                # append data
+                data_byte = data['data']['data'][0]
+                self.frame_data.append( data_byte )
 
         if data["type"] == "stop":
-            # TODO: sanity-check if frame has started
-            # TODO: sanity-check if frame's address has been set
-            # TODO: sanity-check if frame's data is not empty
+            # sanity-check if frame has started
+            if self.frame_started:
+                # TODO: sanity-check if frame's address has been set
+                # TODO: sanity-check if frame's data is not empty
 
-            self.temp_frame["end_time"] = data["end_time"]
-            
-            # TODO: do the real decoding
-            if "I2C address matches": # TODO should depend on I2C_BUS_ADDRESS_SETTING
-                if "isacommand": # FIXME: I2C master writes command to sensor: identified by write direction (address)
+                self.temp_frame['end_time'] = data['end_time']
                 
-                    # sanity-check if this can be a serial: check length
-                    if len( self.frame_data ) is not 2: # commands are 16 bit, i.e. 2 bytes
-                        self.temp_frame["type"] = "error"
-                    else:
-                        self.temp_frame["type"] = "command"
-                        if "getserialcmd": # FIXME: check against b"\x10\x00"
-                            self.temp_frame["data"]["command"] = "get serial number"
-                            self.sensor_mode = getSerial
-                        elif "setmeasurementmodecmd": # FIXME: check against b"\x31\xAE"
-                            self.temp_frame["data"]["command"] = "get measurement"
-                            self.sensor_mode = getMeasurement
-                        elif "softresetcmd": # FIXME: check against b"\x20\x00"
-                            self.temp_frame["data"]["command"] = "soft reset"
-                            self.sensor_mode = None
-                        else:
-                            self.temp_frame["data"]["command"] = "unknown"
-                            self.sensor_mode = None
-                else: # I2C master reads command results from sensor: identified by read direction (address)
-                    if self.sensor_mode = getSerial:
-                        self.temp_frame["type"] = "serialno"
+                # TODO: do the real decoding
+                if "I2C address matches": # TODO should depend on I2C_BUS_ADDRESS_SETTING
+                    if "isacommand": # FIXME: I2C master writes command to sensor: identified by write direction (address)
+                    
                         # sanity-check if this can be a serial: check length
-                        if len( self.frame_data ) is not 6:
-                            self.temp_frame["type"] = "error"
+                        if len( self.frame_data ) is not 2: # commands are 16 bit, i.e. 2 bytes
+                            self.temp_frame['type'] = "error"
                         else:
-                            # TODO: sanity-check if this can be a serial: check CRC
-                            # TODO: add serial number to frame
-                            #       serial number is sent MSByte and MSBit first: serialno[31:16],crc[7:0],serialno[15:0],crc[7:0]
-                            self.temp_frame["data"]["serialno"] = "serial number"
-                    elif self.sensor_mode = getMeasurement:
-                        self.temp_frame["type"] = "measurement"
-                        # sanity-check if this can be a measurement: check length
-                        if len( self.frame_data ) is not 6:
-                            self.temp_frame["type"] = "error"
+                            self.temp_frame['type'] = "command"
+                            if "getserialcmd": # FIXME: check against b"\x10\x00"
+                                self.temp_frame['data']['command'] = "get serial number"
+                                self.sensor_mode = SensorMode.GET_SERIAL
+                            elif "setmeasurementmodecmd": # FIXME: check against b"\x31\xAE"
+                                self.temp_frame['data']['command'] = "get measurement"
+                                self.sensor_mode = SensorMode.GET_MEASUREMENT
+                            elif "softresetcmd": # FIXME: check against b"\x20\x00"
+                                self.temp_frame['data']['command'] = "soft reset"
+                                self.sensor_mode = None
+                            else:
+                                self.temp_frame['data']['command'] = "unknown"
+                                self.sensor_mode = None
+                    else: # I2C master reads command results from sensor: identified by read direction (address)
+                        if self.sensor_mode == SensorMode.GET_SERIAL:
+                            self.temp_frame['type'] = "serialno"
+                            # sanity-check if this can be a serial: check length
+                            if len( self.frame_data ) is not 6:
+                                self.temp_frame['type'] = "error"
+                            else:
+                                # TODO: sanity-check if this can be a serial: check CRC
+                                # TODO: add serial number to frame
+                                #       serial number is sent MSByte and MSBit first: serialno[31:16],crc[7:0],serialno[15:0],crc[7:0]
+                                self.temp_frame['data']['serialno'] = "serial number"
+                        elif self.sensor_mode == SensorMode.GET_MEASUREMENT:
+                            self.temp_frame["type"] = "measurement"
+                            # sanity-check if this can be a measurement: check length
+                            if len( self.frame_data ) is not 6:
+                                self.temp_frame["type"] = "error"
+                            else:
+                                # TODO: sanity-check if this can be a measurement: check CRC
+                                # TODO: check measurement value itself, according to protocol measurement[1:0] is always zero
+                                # TODO: add measurement data to frame
+                                #       measurement data is sent MSByte and MSBit first: measurement[15:0],crc[7:0]
+                                self.temp_frame["data"]["raw"] = 42
+                                self.temp_frame["data"]["flow"] = 23.42
+                                
+                                # TODO: calculate flow from raw value, dependent on type
+                                #       flow [slm] = (measured value - offset flow) / scale factor flow
                         else:
-                            # TODO: sanity-check if this can be a measurement: check CRC
-                            # TODO: check measurement value itself, according to protocol measurement[1:0] is always zero
-                            # TODO: add measurement data to frame
-                            #       measurement data is sent MSByte and MSBit first: measurement[15:0],crc[7:0]
-                            self.temp_frame["data"]["raw"] = 42
-                            self.temp_frame["data"]["flow"] = 23.42
-                            
-                            # TODO: calculate flow from raw value, dependent on type
-                            #       flow [slm] = (measured value - offset flow) / scale factor flow
-                    else:
-                        self.temp_frame["type"] = "unknown"
-                        # TODO: might still want to add/display data
+                            self.temp_frame["type"] = "unknown"
+                            # TODO: might still want to add/display data
 
-                new_frame = temp_frame
-            else:
-                new_frame = None
+                    new_frame = self.temp_frame
+                else:
+                    new_frame = {}
 
-            self.temp_frame = None
-            self.frame_started = False
-            self.frame_data = None
+                self.temp_frame = None
+                self.frame_started = False
+                self.frame_data = None
 
-            return new_frame
+                return new_frame
 
